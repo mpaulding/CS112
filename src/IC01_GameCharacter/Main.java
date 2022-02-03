987dd2260ef66fe85130a583ed18cd4bbc3e15fc
Michael Paulding <mpaulding@users.noreply.github.com>
2022-02-03 13:23:13 -0800

    Initial commit

---
+++ src/IC01_GameCharacter/Main.java
@@ -0,0 +1,8 @@
+package IC01_GameCharacter;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
---
+++ src/IC02_GameCharacterInheritance/Main.java
@@ -0,0 +1,8 @@
+package IC02_GameCharacterInheritance;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
